1633c7b8812144d1219770d5023c10643e5ef350
Nazeer khan <nazeerkhan14official@gmail.com>
2023-07-24 20:23:53 -0700

    commmit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+// IMPORTING COMPONENTS
+import Header from './components/Header'
+import AnimRoutes from './components/AnimRoutes'
+
+// IMPORTING ROUTER
+import { BrowserRouter as Router } from 'react-router-dom';
+// IMPORTING FRAMER MOTION
+  import { motion } from 'framer-motion';
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <Router>
+        <Header/>
+        <AnimRoutes/>
+    </Router>
+    )
 };
 
 export default App;
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { IoMdClose } from "react-icons/io";
+import { CgMenuRight } from "react-icons/cg";
+// Link
+import { Link } from 'react-router-dom';
+// Motion
+import { motion } from 'framer-motion';
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return <nav className='text-3xl cursor-pointer'>
+    <CgMenuRight />
+
+    <div className="absolute right-0 top-0 w-full max-w-xs z-20 bg-white h-screen">
+      {/* Close icon */}
+      <div className='absolute left-4 top-14 text-4xl'>
+        <IoMdClose/>
+      </div>
+
+{/* Mobile Mene */}
+      <ul className='h-full flex flex-col items-center justify-center text-[#696A6D] hover:text-primary transition font-bold font-primary text-3xl gap-y-12'>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/portfolio'}>Portfolio</Link></li>
+        <li><Link to={'/about'}>About</Link></li>
+        <li><Link to={'/contact'}>Contact</Link></li>
+      </ul>
+    </div>
+  </nav>;
 };
 
 export default MobileNav;
